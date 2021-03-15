dc091452005deffac792e897aee03a3a31bc76e3
Mayrinika <mashuliy_u@mail.ru>
2021-03-15 17:21:56 +0500

    add server

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+const port = 9000;
+
+const chatMessages = [];
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+})
+
+app.get('/chatMessages', (req, res) => {
+    res.status(200).json(chatMessages);
+})
+
+app.post('/chatMessages', (req, res) => {
+    chatMessages.push(req.body);
+    res.status(200).json(chatMessages);
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`Example app listening at http://localhost:${port}`);
+})
