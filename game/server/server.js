fa0d2d6f09fe6a17243cfec607bbb782b8bce08e
Mayrinika <mashuliy_u@mail.ru>
2021-03-15 16:02:56 +0500

    API for chat

--- game/server/server.js
+++ game/server/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const app = express();
-const port = 3001;
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const app = express();
+const port = 9000;
+
+const chatMessages = [];
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/chatMessages', (req, res) => {
+    res.status(200).json(chatMessages);
 })
 
-app.listen(port, () => {
+app.post('/chatMessages', (req, res) => {
+    chatMessages.push(req.body);
+    res.status(200).json(chatMessages);
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+
     console.log(`Example app listening at http://localhost:${port}`);
 })
--- game/src/Chat.tsx
+++ game/src/Chat.tsx
@@ -23,17 +23,39 @@ class Chat extends Component<chatProps, chatState> {
             chatMessages: []
         }
     }
+
+    componentDidMount() {
+        fetch('http://localhost:9000/chatMessages')
+            .then(res => res.json())
+            .then(chatMessages => {
+                this.setState({
+                    chatMessages
+                });
+            });
+    }
+
     addMessage = (evt: React.ChangeEvent<HTMLFormElement>) => {
         evt.preventDefault();
         const {inputMessage, chatMessages} = this.state;
         const {currentPlayer} = this.props;
-        this.setState({inputMessage: '', chatMessages: [...chatMessages, {name: currentPlayer, text: inputMessage}]});
-        let json = JSON.stringify({name: currentPlayer, text: inputMessage});
-        console.log(json);
+
+        fetch('http://localhost:9000/chatMessages', {
+            method: 'POST',
+            body: JSON.stringify({name: currentPlayer, text: inputMessage}),
+            headers: {'Content-Type': 'application/json'},
+        })
+            .then(res => {
+                if (res.ok)
+                    this.setState({
+                        inputMessage: '',
+                        chatMessages: [...chatMessages, {name: currentPlayer, text: inputMessage}]
+                    });
+            })
     }
     enterMessage = (evt: React.ChangeEvent<HTMLInputElement>) => {
         this.setState({...this.state, inputMessage: evt.target.value});
     }
+
     render() {
         return (
             <div className="Chat">
@@ -42,7 +64,7 @@ class Chat extends Component<chatProps, chatState> {
                         <p><span className="Chat-message-name">{message.name}: </span>{message.text}</p>
                     ))}
                 </div>
-                <form onSubmit={this.addMessage} >
+                <form onSubmit={this.addMessage}>
                     <label htmlFor="message">ваш ответ: </label>
                     <input
                         id="message"
