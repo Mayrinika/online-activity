e9001e7839adead5516bb92bd100187c0c9b7f4a
Mayrinika <mashuliy_u@mail.ru>
2021-03-25 15:34:20 +0500

    add proxy

--- game/src/utils/routes.tsx
+++ game/src/utils/routes.tsx
@@ -1,15 +1,15 @@
-const serverURL = 'http://localhost:9000/';
-
+//const serverURL = 'http://localhost:9000';
+const serverURL = '/api';
 export default function getRoutes(currentGameId?: string | null) {
     return {
         root: `${serverURL}`,
-        gameId: `${serverURL}${currentGameId}`,
-        chatMessages: `${serverURL}${currentGameId}/chatMessages`,
-        addPlayer: `${serverURL}${currentGameId}/addPlayer`,
-        addImg: `${serverURL}${currentGameId}/addImg`,
-        addWordAndPainter: `${serverURL}${currentGameId}/addWordAndPainter`,
-        clearCountdown: `${serverURL}${currentGameId}/clearCountdown`,
-        setWinner: `${serverURL}${currentGameId}/setWinner`,
-        setTimeIsOver: `${serverURL}${currentGameId}/setTimeIsOver`,
+        gameId: `${serverURL}/${currentGameId}`,
+        chatMessages: `${serverURL}/${currentGameId}/chatMessages`,
+        addPlayer: `${serverURL}/${currentGameId}/addPlayer`,
+        addImg: `${serverURL}/${currentGameId}/addImg`,
+        addWordAndPainter: `${serverURL}/${currentGameId}/addWordAndPainter`,
+        clearCountdown: `${serverURL}/${currentGameId}/clearCountdown`,
+        setWinner: `${serverURL}/${currentGameId}/setWinner`,
+        setTimeIsOver: `${serverURL}/${currentGameId}/setTimeIsOver`,
     }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -26,16 +26,16 @@ const timerIds = {};
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.status(200).send(games);
 })
 
-app.get('/:gameId', (req, res) => {
+app.get('/api/:gameId', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     res.status(200).send(currentGame);
 })
 
-app.post('/:gameId', (req, res) => {
+app.post('/api/:gameId', (req, res) => {
     games.push({
         id: req.params.gameId,
         players: [],
@@ -51,30 +51,30 @@ app.post('/:gameId', (req, res) => {
     res.status(200).send(games);
 })
 
-app.post('/:gameId/addPlayer', (req, res) => {
+app.post('/api/:gameId/addPlayer', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     currentGame.players.push(req.body.player);
     res.status(200).send(games);
 })
 
-app.get('/:gameId/chatMessages', (req, res) => {
+app.get('/api/:gameId/chatMessages', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     res.status(200).send(currentGame.chatMessages);
 })
 
-app.post('/:gameId/chatMessages', (req, res) => {
+app.post('/api/:gameId/chatMessages', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     currentGame.chatMessages.push(req.body);
     res.status(200).send(games);
 })
 
-app.post('/:gameId/addImg', (req, res) => {
+app.post('/api/:gameId/addImg', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     currentGame.img = req.body.img;
     res.status(200).send(games);
 })
 
-app.post('/:gameId/addWordAndPainter', (req, res) => {
+app.post('/api/:gameId/addWordAndPainter', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     if (currentGame.wordToGuess === '') {
         const words = readFileSync("./src/utils/words.txt", 'utf8').split('\r\n');
@@ -89,13 +89,13 @@ app.post('/:gameId/addWordAndPainter', (req, res) => {
     res.status(200).send(games);
 });
 
-app.post('/:gameId/clearCountdown', (req, res) => {
+app.post('/api/:gameId/clearCountdown', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     clearTimeout(timerIds[currentGame.id]);
     res.status(200).send(games);
 })
 
-app.post('/:gameId/setWinner', (req, res) => {
+app.post('/api/:gameId/setWinner', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     currentGame.winner = req.body.winner;
     currentGame.isWordGuessed = true;
@@ -103,7 +103,7 @@ app.post('/:gameId/setWinner', (req, res) => {
     res.status(200).send(games);
 })
 
-app.post('/:gameId/setTimeIsOver', (req, res) => {
+app.post('/api/:gameId/setTimeIsOver', (req, res) => {
     const currentGame = games.find(game => game.id === req.params.gameId);
     currentGame.isTimeOver = true;
     currentGame.isGameOver = true;
