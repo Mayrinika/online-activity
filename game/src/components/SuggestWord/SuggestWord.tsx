d9a3a8bb78e7bf59331391d056064fbc6ef3cd76
Mayrinika <mashuliy_u@mail.ru>
2021-04-07 19:17:15 +0500

    add some style in SuggestWord

--- game/src/components/SuggestWord/SuggestWord.tsx
+++ game/src/components/SuggestWord/SuggestWord.tsx
@@ -1,10 +1,19 @@
 import {RouteComponentProps} from "react-router-dom";
 import React, {Component} from "react";
-import './SuggestWord.css';
+import {v4 as uuidv4} from 'uuid';
+//components
+//utils
 import getRoutes from "../../utils/routes";
-import { v4 as uuidv4 } from 'uuid';
+//styles
+//import './SuggestWord.css';
+import {withStyles, WithStyles} from "@material-ui/core/styles";
+import {Button, Container, Grid, Typography, TextField, Box} from '@material-ui/core';
 
-const ws= new WebSocket('ws://localhost:8080');
+const styles = (theme: { content: any; }) => ({
+    ...theme.content,
+});
+
+const ws = new WebSocket('ws://localhost:8080');
 
 interface SuggestedWord {
     id: string;
@@ -19,7 +28,8 @@ interface SuggestWordState {
     enteredWord: string;
     words: SuggestedWord[];
 }
-interface SuggestWordProps extends RouteComponentProps {
+
+interface SuggestWordProps extends RouteComponentProps, WithStyles<typeof styles> {
 
 }
 
@@ -29,81 +39,110 @@ class SuggestWord extends Component<SuggestWordProps, SuggestWordState> {
         this.state = {
             enteredWord: '',
             words: [],
-        }
+        };
     }
+
     componentDidMount() {
         this.getWordsFromServer();
         ws.onmessage = (response: any) => {
             this.setState({words: JSON.parse(response.data)});
-        }
+        };
     }
+
     sendWord = (evt: React.ChangeEvent<HTMLFormElement>) => {
         evt.preventDefault();
-        ws.send(JSON.stringify({'messageType':'sendSuggestedWord', 'word':this.state.enteredWord, 'id':uuidv4()}));
+        ws.send(JSON.stringify({'messageType': 'sendSuggestedWord', 'word': this.state.enteredWord, 'id': uuidv4()}));
         this.setState({enteredWord: ''});
-    }
+    };
     enterWord = (evt: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ ...this.state, enteredWord: evt.target.value });
-    }
-    getWordsFromServer = async() => {
+        this.setState({...this.state, enteredWord: evt.target.value});
+    };
+    getWordsFromServer = async () => {
         const res = await fetch(getRoutes().suggestedWords);
         const data = await res.text();
         const words = JSON.parse(data);
         this.setState({words});
-    }
+    };
     likeWord = (wordId: string) => {
-        ws.send(JSON.stringify({'messageType':'likeWord', 'wordId':wordId, 'author':localStorage.getItem('playerName')}));
-    }
+        ws.send(JSON.stringify({
+            'messageType': 'likeWord',
+            'wordId': wordId,
+            'author': localStorage.getItem('playerName')
+        }));
+    };
     dislikeWord = (wordId: string) => {
-        ws.send(JSON.stringify({'messageType':'dislikeWord', 'wordId':wordId, 'author':localStorage.getItem('playerName')}));
-    }
+        ws.send(JSON.stringify({
+            'messageType': 'dislikeWord',
+            'wordId': wordId,
+            'author': localStorage.getItem('playerName')
+        }));
+    };
+
     render() {
+        const {words, enteredWord} = this.state;
         let wordStatusHasChanged;
-        if (this.state.words.some((word: SuggestedWord) => word.isApproved)) {
-            const approvedWord = this.state.words.find(word => word.isApproved);
+        if (words.some((word: SuggestedWord) => word.isApproved)) {
+            const approvedWord = words.find(word => word.isApproved);
             // @ts-ignore
-            wordStatusHasChanged = `Слово ${approvedWord.word} было одобрено тремя игроками и попало в наш словарь!`
+            wordStatusHasChanged = `Слово ${approvedWord.word} было одобрено тремя игроками и попало в наш словарь!`;
         }
-        if (this.state.words.some((word: SuggestedWord) => word.isDeclined)) {
-            const declinedWord = this.state.words.find(word => word.isDeclined);
+        if (words.some((word: SuggestedWord) => word.isDeclined)) {
+            const declinedWord = words.find(word => word.isDeclined);
             // @ts-ignore
-            wordStatusHasChanged = `Слово ${declinedWord.word} было удалено, так как не понравилось трем игрокам`
+            wordStatusHasChanged = `Слово ${declinedWord.word} было удалено, так как не понравилось трем игрокам`;
         }
+        const {classes} = this.props;
         return (
-            <div className="SuggestWord">
-                <form className="SuggestWord-from" onSubmit={this.sendWord}>
-                    <label htmlFor="wordInput">Предложите слово для игр:</label>
-                    <input
-                        type="text"
-                        id="wordInput"
-                        value={this.state.enteredWord}
-                        onChange={this.enterWord}
-                    />
-                    <input type='submit' />
-                </form>
-                <div className="SuggestWord-words-list">
-                    {this.state.words.map((word) => (
-                        <div key={word.id} className="SuggestWord-word">
-                            {word.word}
-                            <div className="SuggestWord-buttons">
-                                <button
-                                    // @ts-ignore
-                                    className={`"SuggestedWord-button SuggestWord-button-plus" ${word.likes.includes(localStorage.getItem('playerName')) ? "active" : ""}`}
-                                    onClick={() => this.likeWord(word.id)}>+
-                                </button>
-                                <button
-                                    // @ts-ignore
-                                    className={`"SuggestedWord-button SuggestWord-button-plus" ${word.dislikes.includes(localStorage.getItem('playerName')) ? "active" : ""}`}
-                                    onClick={() => this.dislikeWord(word.id)}>-
-                                </button>
-                                <div>{word.likes.length - word.dislikes.length}</div>
+            <Container className={classes.outerContainer} maxWidth='lg' style={{height: 500}}>
+                <Box >
+                    <form onSubmit={this.sendWord} className={classes.innerContainer}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="wordInput"
+                            label="Введите слово"
+                            name="word"
+                            autoFocus
+                            onChange={this.enterWord}
+                            value={enteredWord}
+                        />
+                        <Button
+                            className={classes.button}
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            size="large"
+                        >
+                            Предложить
+                        </Button>
+                    </form>
+                    <Box>
+                        {this.state.words.map((word) => (
+                            <div key={word.id} className="SuggestWord-word">
+                                {word.word}
+                                <div className="SuggestWord-buttons">
+                                    <button
+                                        // @ts-ignore
+                                        className={`"SuggestedWord-button SuggestWord-button-plus" ${word.likes.includes(localStorage.getItem('playerName')) ? "active" : ""}`}
+                                        onClick={() => this.likeWord(word.id)}>+
+                                    </button>
+                                    <button
+                                        // @ts-ignore
+                                        className={`"SuggestedWord-button SuggestWord-button-plus" ${word.dislikes.includes(localStorage.getItem('playerName')) ? "active" : ""}`}
+                                        onClick={() => this.dislikeWord(word.id)}>-
+                                    </button>
+                                    <div>{word.likes.length - word.dislikes.length}</div>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                    <p>{wordStatusHasChanged}</p>
-                </div>
-            </div>
-        )
+                        ))}
+                        <p>{wordStatusHasChanged}</p>
+                    </Box>
+                </Box>
+            </Container>
+        );
     }
 }
-export default SuggestWord;
+
+export default (withStyles(styles)(SuggestWord));
