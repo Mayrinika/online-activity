b509383494ed5a6b5f9af1866a1ba142994f8a06
Mayrinika <mashuliy_u@mail.ru>
2021-04-16 22:38:51 +0500

    refactoring

--- game/src/components/Api/ApiProvider.tsx
+++ game/src/components/Api/ApiProvider.tsx
@@ -35,12 +35,12 @@ class ApiMethods implements Api {
     };
 
     getUserLoginData = async (): Promise<UserLoginData> => {
-        // return await fetch(getRoutes().login)
-        //     .then(res => res.json())
-        //     .catch(err => this.checkStatus(err));
-        const res = await fetch(getRoutes().login);
-        const data = await res.text();
-        return JSON.parse(data);
+        return await fetch(getRoutes().login)
+            .then(res => res.json())
+            .catch(err => this.checkStatus(err));
+        // const res = await fetch(getRoutes().login);
+        // const data = await res.text();
+        // return JSON.parse(data);
     };
 
     signup = async (name: string, password: string, avatar: string | ArrayBuffer | null): Promise<User | undefined> => {
@@ -57,17 +57,17 @@ class ApiMethods implements Api {
                 if (this.user === undefined)
                     throw new Error('Failed to get user');
             })
-            //.catch(err => this.checkStatus(err));
+            .catch(err => this.checkStatus(err));
         return this.user;
     };
 
     getAllUsers = async (): Promise<User[]> => {
-        const res = await fetch(getRoutes().signup);
-        const data = await res.text();
-        return JSON.parse(data);
-        // return await fetch(getRoutes().signup)
-        //     .then(res => res.json())
-        //     .catch(err => this.checkStatus(err));
+        // const res = await fetch(getRoutes().signup);
+        // const data = await res.text();
+        // return JSON.parse(data);
+        return await fetch(getRoutes().signup)
+            .then(res => res.json())
+            .catch(err => this.checkStatus(err));
     };
 
     login = async (name: string, password: string): Promise<User | undefined> => {
@@ -84,32 +84,32 @@ class ApiMethods implements Api {
                 if (this.user === undefined)
                     throw new Error('Failed to get user');
             })
-            //.catch(err => this.checkStatus(err)); //TODO
+        .catch(err => this.checkStatus(err));
         return this.user;
     };
 
     checkAuthorization = async (): Promise<void> => {
-        // await fetch('/cookie-auth-protected-route', {credentials: 'include'})
-        //     .catch(err => this.checkStatus(err));
-        await fetch('/cookie-auth-protected-route', {credentials: 'include'});
+        await fetch('/cookie-auth-protected-route', {credentials: 'include'})
+            .catch(err => this.checkStatus(err));
+        //await fetch('/cookie-auth-protected-route', {credentials: 'include'});
     };
 
     getAllGames = async (): Promise<GameType[]> => {
-        // return await fetch(getRoutes().app)
-        //     .then(res => res.json())
-        //     .catch(err => this.checkStatus(err));
-        const res = await fetch(getRoutes().app);
-        const data = await res.text();
-        return JSON.parse(data);
+        return await fetch(getRoutes().app)
+            .then(res => res.json())
+            .catch(err => this.checkStatus(err));
+        // const res = await fetch(getRoutes().app);
+        // const data = await res.text();
+        // return JSON.parse(data);
     };
 
     getGame = async (): Promise<GameType> => {
-        // return await fetch(getRoutes(this._gameId).gameId)
-        //     .then(res => res.json())
-        //     .catch(err => this.checkStatus(err));
-        const res = await fetch(getRoutes(this._gameId).gameId);
-        const data = await res.text();
-        return JSON.parse(data);
+        return await fetch(getRoutes(this._gameId).gameId)
+            .then(res => res.json())
+            .catch(err => this.checkStatus(err));
+        // const res = await fetch(getRoutes(this._gameId).gameId);
+        // const data = await res.text();
+        // return JSON.parse(data);
     };
 
     clearCountdown = async (): Promise<void> => {
@@ -119,7 +119,7 @@ class ApiMethods implements Api {
                 'Content-Type': 'application/json;charset=utf-8'
             }
         })
-            //.catch(err => this.checkStatus(err));
+        .catch(err => this.checkStatus(err));
     };
 
     sendLineToServer = async (line: any): Promise<void> => {
@@ -130,28 +130,27 @@ class ApiMethods implements Api {
             },
             body: JSON.stringify({line})
         })
-            //.catch(err => this.checkStatus(err));
+        .catch(err => this.checkStatus(err));
     };
 
     getLeaderboardDataFromServer = async (): Promise<[userId: string, score: number][]> => {
-        const res = await fetch(getRoutes().leaderboard);
-        const data = await res.text();
-        return  JSON.parse(data);
-       
-        // const leaderboard = await fetch(getRoutes().leaderboard)
-        //     .then(res => res.json())
-        //     .catch(err => this.checkStatus(err));
-        // return leaderboard.sort((item1: { player: Player, score: number }, item2: { player: Player, score: number }) =>
-        //     item2.score - item1.score);
+        // const res = await fetch(getRoutes().leaderboard);
+        // const data = await res.text();
+        // return  JSON.parse(data);
+
+        return await fetch(getRoutes().leaderboard)
+            .then(res => res.json())
+            .catch(err => this.checkStatus(err));
     };
 
     getSuggestWordsFromServer = async (): Promise<SuggestedWord[]> => {
-        // return await fetch(getRoutes().suggestedWords)
-        //     .then(res => res.json())
-        //     .catch(err => this.checkStatus(err));
-        const res = await fetch(getRoutes().suggestedWords);
-        const data = await res.text();
-        return JSON.parse(data);
+        return await fetch(getRoutes().suggestedWords)
+            .then(res => res.json())
+            .catch(err => this.checkStatus(err));
+
+        // const res = await fetch(getRoutes().suggestedWords);
+        // const data = await res.text();
+        // return JSON.parse(data);
     };
 
     deleteLine = async (): Promise<void> => {
@@ -159,7 +158,7 @@ class ApiMethods implements Api {
             method: 'POST',
             headers: {'Content-Type': 'application/json'}
         })
-            //.catch(err => this.checkStatus(err));
+        .catch(err => this.checkStatus(err));
     };
 
     private async checkStatus(response: Response): Promise<void> {
